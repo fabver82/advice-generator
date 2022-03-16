9ef91a80bef99fe02bbb064bc2a576fcb321bbc7
Fabien V <fabien.vermeiren@gmail.com>
2022-03-16 13:11:54 +0100

    js completed for random quote

--- public/js/random-quote.js
+++ public/js/random-quote.js
@@ -1,2 +1,18 @@
 let button = document.getElementById('button');
-let quoteField = document.getElementsByClassName('quote');
+let quoteField = document.querySelector('.quote');
+let quoteIdField = document.querySelector('.quote-id');
+console.log(quoteField);
+
+const randomQuote = async function(){
+    return await fetch('https://api.adviceslip.com/advice')
+    .then(quote=>quote.json());
+}
+
+button.addEventListener('click',async ()=>{
+
+    await randomQuote().then((quote)=>{
+        console.log(quoteIdField);
+        quoteIdField.textContent = `ADVICE #${quote.slip.id}`;
+        quoteField.textContent = quote.slip.advice;
+    })
+})
